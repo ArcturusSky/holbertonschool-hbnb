02fa2fd89b3f1f346d61f75592ce27e42ca07c96
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-10-22 20:20:57 +0200

    Update: Enhance Place model with validations and attribute refinements
    - Imported validation functions: place_validation, owner_validation, description_validation
    - Added validations for placename, description, and owner
    - Changed 'title' attribute to 'placename'
    - Created 'adress' tuple from latitude and longitude
    - Renamed 'reviews' to 'myreviews' and 'amenities' to 'myamenities'
    - Updated __init__ method docstring

--- app/models/place.py
+++ app/models/place.py
@@ -1,15 +1,18 @@
 from base_model import BaseModel
 # from validation_checks import (for later implementation)
 from PseudoDataBase import places_id_list
+from validation_checks import place_validation, owner_validation, description_validation
+
+
 class Place(BaseModel):
     """
     Represents a Place with attributes inherited from BaseModel.
     """
 
     # Initialisation of the Place class
-    def __init__(self, title, description, price, latitude, longitude, owner):
+    def __init__(self, placename, description, price, latitude, longitude, owner):
         """
-        Create instance of review.
+        Create instance of place.
         """
 
         # Get attributes from super class (BaseModel)
@@ -18,19 +21,34 @@ class Place(BaseModel):
         # Add the ID (from the BaseModel) to the place id list.
         places_id_list.append(id)
 
-        self.title = title
-        self.description = description
-        self.price = price
-        self.latitude = latitude
-        self.longitude = longitude
-        self.owner = owner
-        self.reviews = []  # List to store related reviews
-        self.amenities = []  # List to store related amenities
+        # Create tuple variable with latitude and longitude
+        self.adress = (latitude, longitude)
 
+        # Check if placename is valid
+        place_validation(placename, self.adress)
+        self.placename = placename
+
+        # Check if description is valid
+        description_validation(description)
+        self.description = description
+
+
+        self.price = price
+
+        # Check if owner exist
+        owner_validation(owner)
+        self.owner = owner
+
+
+        self.myreviews = []  # List to store related reviews
+        self.myamenities = []  # List to store related amenities
+
+
+    # Relationships with others classes and adding datas
     def add_review(self, review):
         """Add a review to the place."""
-        self.reviews.append(review)
+        self.myreviews.append(review)
 
     def add_amenity(self, amenity):
         """Add an amenity to the place."""
-        self.amenities.append(amenity)
+        self.myamenities.append(amenity)
