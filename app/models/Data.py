903887684f037e4dd139ad63f6ce4c3ce86b5193
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-10-22 15:24:07 +0200

    Added general ID list to store ID for each class

--- app/models/Data.py
+++ app/models/Data.py
@@ -2,6 +2,12 @@
 Module that will holds all the Data while waiting for the SQL
 """
 
+# General
+users_id_list = []
+places_id_list = []
+review_id_list = []
+amenity_id_list = []
+
 # Users
 username_list = []
 phonenumber_list = []
