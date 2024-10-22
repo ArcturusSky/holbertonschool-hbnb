1d015be5e48ad0cfb7607098c9c9aa677d7e92f9
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-10-22 10:44:29 +0200

    Create: Ghost database

---
+++ app/models/Data.py
@@ -0,0 +1,15 @@
+"""
+Module that will holds all the Data while waiting for the SQL
+"""
+
+# Users
+username_list = []
+phonenumber_list = []
+email_list = []
+
+# Places
+placename_list = []
+
+# Reviews
+
+# Amenities
