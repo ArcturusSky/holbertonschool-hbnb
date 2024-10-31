2b8a80019e64c19faf3961e556b0211f206ed843
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-10-31 01:41:36 +0100

    Setting namespaces for each route

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import user_api as user_namespace
+from app.api.v1.amenities import amenity_api as amenity_namespace
+from app.api.v1.places import place_api as place_namespace
+from app.api.v1.reviews import review_api as review_namespace
 
 def create_app():
     app = Flask(__name__)
@@ -8,4 +11,7 @@ def create_app():
 
     # Register the users namespace
     api.add_namespace(user_namespace, path='/api/v1/users')
+    api.add_namespace(amenity_namespace, path='/api/v1/amenities')
+    api.add_namespace(place_namespace, path='/api/v1/places')
+    api.add_namespace(review_namespace, path='/api/v1/reviews')
     return app
--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -7,18 +7,14 @@ api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
 # Initialize the API object, defining version and basic metadata
 api = Api(api_bp, version='1.0', title="HBnB API", description="API for HBnB project")
 
-# Import the user namespace for handling user-related operations
+# Import namespace for handling operations
 from .v1.users import user_api as user_namespace
+from .v1.places import place_api as place_namespace
+from .v1.reviews import review_api as review_namespace
+from .v1.amenities import amenity_api as amenity_namespace
 
-    # ////////// Import other namespaces as needed for different resources //////////
-# from .v1.places import place_api as place_namespace
-# from .v1.reviews import review_api as review_namespace
-# from .v1.amenities import amenity_api as amenity_namespace
-
-# Add the user namespace to the API, defining its URL path
+# Add namespace to the API, defining its URL path
 api.add_namespace(user_namespace, path='/users')
-
-    # ////////// Add other namespaces to the API for different resource paths //////////
-# api.add_namespace(places_namespace, path='/places')
-# api.add_namespace(reviews_namespace, path='/reviews')
-# api.add_namespace(amenities_namespace, path='/amenities')
+api.add_namespace(place_namespace, path='/places')
+api.add_namespace(review_namespace, path='/reviews')
+api.add_namespace(amenity_namespace, path='/amenities')
