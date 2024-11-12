12b9c47aeed4d964d4149180490284831be24b4f
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-11-12 10:06:26 +0100

    [REFACTOR]: Enhance app creation with configurable settings
    - Modified create_app function to accept a configuration class parameter:
      - Added default config_class argument "config.DevelopmentConfig"
      - Implemented app.config.from_object() to apply configuration
    
    - Improved code documentation:
      - Added detailed docstring for create_app function
      - Included description of function arguments and return value
      - Added comments explaining key steps in app creation process
    
    - Reorganized imports:
      - Grouped core Flask imports
      - Separated entity-specific namespace imports with a comment

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,12 +1,31 @@
 from flask import Flask
 from flask_restx import Api
+
+# Routes/namespaces for each entity
 from app.api.v1.users import user_api as user_namespace
 from app.api.v1.amenities import amenity_api as amenity_namespace
 from app.api.v1.places import place_api as place_namespace
 from app.api.v1.reviews import review_api as review_namespace
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
+    """
+    Fonction that create the app
+    
+    Args:
+        config_class (str): Path to the configuration class to use
+                           by default: "config.DevelopmentConfig" since we are in dev currently
+    
+    Returns:
+        Flask: Flask instance configured as wanted
+    """
+
+    # Create an instance of Flask
     app = Flask(__name__)
+
+    # Apply config (by default DevelopmentConfig)
+    app.config.from_object(config_class)
+
+    # Initialization of the API and its documentation
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     # Register the users namespace
